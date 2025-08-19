80354cb0fa41f696ce15b9faf335f65638afd1f3
Bekkks <bekkxan.14@gmail.com>
2025-08-19 15:19:05 +0300

    1.12

--- 1.12/main.go
+++ 1.12/main.go
@@ -1,1 +1,20 @@
-package __12
+package main
+
+import "fmt"
+
+func main() {
+	arr := []string{"cat", "cat", "dog", "cat", "tree", "dog"}
+	m := make(map[string]struct{})
+	for _, v := range arr {
+		m[v] = struct{}{}
+	}
+	for l := range m {
+		fmt.Println(l)
+	}
+}
+
+/*Имеется последовательность строк: ("cat", "cat", "dog", "cat", "tree"). Создать для неё собственное множество.
+
+Ожидается: получить набор уникальных слов. Для примера, множество = {"cat", "dog", "tree"}.
+
+*/
