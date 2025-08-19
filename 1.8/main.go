96dd90242e3a3476e27e2ba2f9596fc76fadd06d
Bekkks <bekkxan.14@gmail.com>
2025-08-19 14:37:04 +0300

    1.8

--- 1.8/main.go
+++ 1.8/main.go
@@ -1,1 +1,18 @@
-package __8
+package main
+
+import "fmt"
+
+func Setbit(i int, bit int, num int64) int64 {
+	if bit == 1 {
+		return num | (1 << i)
+	} else {
+		return num &^ (1 << i)
+	}
+}
+
+func main() {
+	var i, bit int
+	var num int64
+	fmt.Scan(&num, &i, &bit)
+	fmt.Println(Setbit(i, bit, num))
+}
