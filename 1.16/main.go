e711bd4df3e33063c7b78c5c25d2c31e9e803a35
Bekkks <bekkxan.14@gmail.com>
2025-08-20 12:52:11 +0300

    1.16

--- 1.16/main.go
+++ 1.16/main.go
@@ -1,1 +1,35 @@
-package __16
+package main
+
+import (
+	"fmt"
+)
+
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	pivot := arr[len(arr)/2]
+	var less, greater []int
+	for i, v := range arr {
+		if i == len(arr)/2 {
+			continue
+		}
+		if v <= pivot {
+			less = append(less, v)
+		} else {
+			greater = append(greater, v)
+		}
+	}
+	quickLess := quickSort(less)
+	quickGreater := quickSort(greater)
+	result := make([]int, 0, len(quickLess)+1+len(quickGreater))
+	result = append(result, quickLess...)
+	result = append(result, pivot)
+	result = append(result, quickGreater...)
+	return result
+}
+
+func main() {
+	arr := []int{3, 2, 1, 5, 6, 4, 3}
+	fmt.Println(quickSort(arr))
+}
